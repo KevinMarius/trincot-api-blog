e6e6c1c0a18077b6d4a05b16456272a6f409609c
KevinMarius <35264130+KevinMarius@users.noreply.github.com>
2024-08-02 15:17:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ console.log(process.env.DB_PASSWORD)
 
 //===================== db connect ==========================
 mongoose
-.connect(`mongodb+srv://katekema1996:K@tekem@_1996@trincot.f6cyeqa.mongodb.net/trincot?retryWrites=true&w=majority`)
+.connect(`mongodb+srv://${proccess.env.DB_USER}:${proccess.env.DB_PASSWORD}@trincot.f6cyeqa.mongodb.net/trincot?retryWrites=true&w=majority`)
 .then(() => console.log("db connected", bcrypt.hashSync("Password0000", 10)))
 
 mongoose.connection.on(("error"), err => {
